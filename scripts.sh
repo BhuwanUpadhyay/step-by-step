b2e59517549d3437d66af6cca6d7b91f64ad55b1
Bhuwan Prasad Upadhyay <40879634+BhuwanUpadhyay@users.noreply.github.com>
2021-06-15 06:22:46 +0545

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -1,1 +1,22 @@
+# Port-forward using json path
+###################################
+#!/bin/bash
 
+set -e
+
+proxyKibana() {
+
+  POD=$(kubectl get pod -n logging -l app=kibana -o jsonpath='{.items[0].metadata.name}')
+
+  kubectl port-forward $POD -n logging 5601:5601
+
+}
+
+proxyPostgres() {
+
+  POD=$(kubectl get pod -n prod -l app.kubernetes.io/name=postgresql -o jsonpath='{.items[0].metadata.name}')
+
+  kubectl port-forward $POD -n prod 5432:5432
+
+}
+#######################################
